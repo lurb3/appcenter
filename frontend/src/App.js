3548803ab8ba15be410dd9bc924c131fda13011d
gustavo <gustavo.gigante.s@gmail.com>
2022-11-01 22:15:11 +0000

    User signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,14 @@ import {
   Route,
 } from 'react-router-dom';
 import Login from 'pages/Login/Login';
+import Signup from 'pages/Signup/Signup';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Login /> }>
-        </Route>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/signup" element={ <Signup /> } />
       </Routes>
     </Router>
   );
--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import apiUtil from 'utils/api';
 import { setJwt } from 'utils/jwt';
 import { TextField, Button } from '@mui/material';
-import './login.scss';
 
-const Login = () => {
+const Signup = () => {
+  const [ userName, setUserName ] = useState('');
   const [ userEmail, setUserEmail ] = useState('');
   const [ userPassword, setUserPassword ] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    apiUtil().post('/user/login', { email: userEmail, password: userPassword })
+    apiUtil().post('/user', { name: userName, email: userEmail, password: userPassword })
       .then((res) => {
         if (res.data.token) setJwt(res.data.token);
       });
@@ -20,13 +20,14 @@ const Login = () => {
   return (
     <div className='formWrapper'>
       <form className='formFields' onSubmit={handleSubmit}>
+        <TextField id="outlined-basic" label="Name" variant="outlined" onChange={(e) => setUserName(e.target.value)} />
         <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setUserEmail(e.target.value)} />
         <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={(e) => setUserPassword(e.target.value)} />
-        <Link to="/signup">Signup</Link>
-        <Button variant="contained" type='submit'>Login</Button>
+        <Link to="/">Login</Link>
+        <Button variant="contained" type='submit'>Signup</Button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
