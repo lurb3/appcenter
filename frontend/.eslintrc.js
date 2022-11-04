4e01485c8bc1285209a1120cb4ea551c617fe68c
Gustavo Monteiro <gustavo.gigante.s@gmail.com>
2022-11-04 23:30:12 +0000

    dev - master (#13)
    * Fe/login UI (#9)
    
    * Readme update
    
    * Login, Signup UI. ShoppingList draft UI. Updated eslint
    
    * Forgotten commits
    
    * Fe/login UI (#11)
    
    * Readme update
    
    * Login, Signup UI. ShoppingList draft UI. Updated eslint
    
    * Forgotten commits
    
    * eslint fix stuff
    
    * Fe/login UI (#12)
    
    * Readme update
    
    * Login, Signup UI. ShoppingList draft UI. Updated eslint
    
    * Forgotten commits
    
    * eslint fix stuff
    
    * small ui changes to shoppinglist

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  "settings": {
+      "react": {
+          "version": "detect"
+      }
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    "object-curly-spacing": ["warn", "always"],
+    "array-bracket-spacing": ["warn", "always"],
+    "indent": ["warn", 2],
+    "max-len": "off",
+  },
+};
