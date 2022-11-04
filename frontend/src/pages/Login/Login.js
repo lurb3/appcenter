f07cc609b5c8cf4734df7cbfc912a70334464d0a
gustavo <gustavo.gigante.s@gmail.com>
2022-11-04 00:13:37 +0000

    Login, Signup UI. ShoppingList draft UI. Updated eslint

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import apiUtil from 'utils/api';
-import { setJwt } from 'utils/jwt';
+import { getJwt, setJwt } from 'utils/jwt';
 import { TextField, Button } from '@mui/material';
+import history from 'utils/history';
 import './login.scss';
 
 const Login = () => {
   const [ userEmail, setUserEmail ] = useState('');
   const [ userPassword, setUserPassword ] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    apiUtil().post('/user/login', { email: userEmail, password: userPassword })
-      .then((res) => {
-        if (res.data.token) setJwt(res.data.token);
-      });
+    const response = await apiUtil().post('/user/login', { email: userEmail, password: userPassword });
+    if (response.token) setJwt(response.token);
+    history.push('/shoppinglist');
   };
 
+  useEffect(() => {
+    const token = getJwt();
+    if (token) history.push('/shoppinglist');
+  }, []);
+
   return (
-    <div className='formWrapper'>
-      <form className='formFields' onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setUserEmail(e.target.value)} />
-        <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={(e) => setUserPassword(e.target.value)} />
-        <Link to="/signup">Signup</Link>
-        <Button variant="contained" type='submit'>Login</Button>
-      </form>
+    <div className='loginWrapper'>
+      <h1 className='colorWhite'>Login page</h1>
+      <div className='formWrapper'>
+        <form className='formFields' onSubmit={handleSubmit}>
+          <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setUserEmail(e.target.value)} />
+          <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={(e) => setUserPassword(e.target.value)} />
+          <Button variant="contained" type='submit'>Login</Button>
+          <Link className='signupLink' to="/signup">Create new account</Link>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { getJwt } from 'utils/jwt';
+import history from './history';
+import { getJwt, clearJwt } from 'utils/jwt';
 
 const apiUtil = () => {
   const api = axios.create({
@@ -10,6 +11,23 @@ const apiUtil = () => {
     config.headers.authorization = `Bearer ${getJwt()}`;
     return config;
   });
+
+  api.interceptors.response.use(
+    (response) => {
+      console.log('Api response: ', response.data);
+      return response.data;
+    },
+    (error) => {
+      const response = error.response;
+      console.log(response.status);
+      if (response.status === 401) {
+        clearJwt();
+        history.push('/');
+      }
+      console.log('API response error: ', error.response);
+      // Reset stuff, do what you want
+    },
+  );
 
   return api;
 };
