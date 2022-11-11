5e551f01fea17282a48b08354a766415edf0a09b
gustavo <gustavo.gigante.s@gmail.com>
2022-11-11 20:58:08 +0000

    Products priority

--- backend/migrations/20221111161509-update-product-lists-priority.js
+++ backend/migrations/20221111161509-update-product-lists-priority.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   async up(db, client) {
-    return db.collection('products').updateMany({priority: {$exists:false}}, { $set: { priority: "Medium" } });
+    return db.collection('products').updateMany({}, { $set: { priority: 2 } });
   },
 
   async down(db, client) {
